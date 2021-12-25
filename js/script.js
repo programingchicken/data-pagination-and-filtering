59c9b5d6492b0f4944fa774c22757834f8092abb
programingchicken <aquacrazz.gp@gmail.com>
2021-12-25 13:06:54 -0800

    fixed

--- js/script.js
+++ js/script.js
@@ -1,113 +1,132 @@
 /*
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
-*/    
- const LIfullStudentlist = document.querySelector('.student-list').children;
+*/
+
+const LIfullStudentlist = document.querySelector('.student-list').children;
 const Profile = 'profile image'
 const maxStudentsPerPage = 9;
 const listStudent = document.querySelector('.student-list');
 const ul = document.querySelector('.link-list')
 
+
+//insert element after another element
 function insertAfter(referenceNode, newNode) {
-  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
+
+//Create the new element
 function CreateNewElementChild(nameOfElement, elementClassName) {
    const newElement = document.createElement(nameOfElement);
-   newElement.className += elementClassName; 
+   newElement.className += elementClassName;
    return newElement;
 }
 
-function createPageButton(stringNumber){
+//Craete a button element
+function createPageButton(stringNumber) {
    const button = CreateNewElementChild('button')
-     button.setAttribute('type', 'button')
-     button.textContent = stringNumber
-     return button;
+   button.setAttribute('type', 'button')
+   button.textContent = stringNumber
+   return button;
 }
 
 
-function createStudent( avatar, title, firstName, lastName, email, date, age) {
-   const li = CreateNewElementChild('li', 'student-item' )
-      document.querySelector('.student-list').appendChild(li);
-     const div = CreateNewElementChild('div', 'student-details' )
-      li.appendChild(div);
-      var img = CreateNewElementChild('img', 'avatar')
-            img.setAttribute('src', avatar)
-            img.setAttribute('alt', Profile)
-        div.appendChild(img);
+//Create the Student elements
+function createStudent(avatar, title, firstName, lastName, email, date, age) {
+   const li = CreateNewElementChild('li', 'student-item')
+   document.querySelector('.student-list').appendChild(li);
+   const div = CreateNewElementChild('div', 'student-details')
+   li.appendChild(div);
+   var img = CreateNewElementChild('img', 'avatar')
+   img.setAttribute('src', avatar)
+   img.setAttribute('alt', Profile)
+   div.appendChild(img);
 
-     let name = CreateNewElementChild('h3', 'name')
-      name.textContent += `${firstName} ${lastName}`;
-       img.parentNode.insertBefore(name, img.nextSibling);
+   let name = CreateNewElementChild('h3', 'name')
+   name.textContent += `${firstName} ${lastName}`;
 
-      let span = CreateNewElementChild('span', 'email')
-      span.textContent += email
-         name.parentNode.insertBefore(span, name.nextSibling);
-     const divJoin = CreateNewElementChild('div', 'joined-details')
-              div.parentNode.insertBefore(divJoin, div.nextSibling);
-      let spanDate = CreateNewElementChild('span', 'date')
-              divJoin.parentNode.insertBefore(spanDate, divJoin.nextSibling);
-        spanDate.textContent += date
-        return li;
+   img.parentNode.insertBefore(name, img.nextSibling);
+
+   let span = CreateNewElementChild('span', 'email')
+   span.textContent += email
+   name.parentNode.insertBefore(span, name.nextSibling);
+   const divJoin = CreateNewElementChild('div', 'joined-details')
+   div.parentNode.insertBefore(divJoin, div.nextSibling);
+   let spanDate = CreateNewElementChild('span', 'date')
+   divJoin.parentNode.insertBefore(spanDate, divJoin.nextSibling);
+   spanDate.textContent += date
+   return li;
 }
 
 
 
 
- 
+
 function showPage(list, page) {
-       const firstIndex = parseInt((page) * 9) - 9;
-       console.log(firstIndex)
-      const lastIndex = (parseInt(page) * 9) - 1; 
-      console.log(lastIndex) 
-      let fullstudentlist;
+   const firstIndex = parseInt((page) * 9) - 9;
+   console.log(firstIndex)
+   const lastIndex = (parseInt(page) * 9) - 1;
+   console.log(lastIndex)
+   let fullstudentlist;
 
-   	for(let i = 0; i < data.length; i++) {
-		 fullStudentlist = createStudent(data[i].picture.large, data[i].name.title, data[i].name.first, data[i].name.last, data[i].email, data[i].registered.date, data[i].registered.age)
+   for (let i = 0; i < data.length; i++) {
+      fullStudentlist = createStudent(data[i].picture.large, data[i].name.title, data[i].name.first, data[i].name.last, data[i].email, data[i].registered.date, data[i].registered.age)
       console.log(fullStudentlist.length);
       LIfullStudentlist[i].style.display = 'none';
-	}
-    for (let i = firstIndex; i <= lastIndex; i++) {
+   }
+   for (let i = firstIndex; i <= lastIndex; i++) {
       LIfullStudentlist[i].style.display = '';
-      }
-   
-  
-     for(let i = 0; i <  list.length/maxStudentsPerPage; i++){
+   }
+
+
+   for (let i = 0; i < list.length / maxStudentsPerPage; i++) {
       console.log(i)
-       const li = CreateNewElementChild('li', `button${i}`)
-        document.querySelector('.link-list').appendChild(li);
-       const button = createPageButton(`${i + 1}`)
-       document.querySelector(`.button${i}`).appendChild(button);
-     }
+      const li = CreateNewElementChild('li', `button${i}`)
+      document.querySelector('.link-list').appendChild(li);
+      const button = createPageButton(`${i + 1}`)
+      document.querySelector(`.button${i}`).appendChild(button);
+   }
 
-    const button = document.querySelector(`.button${page - 1}`).children[0]
-     button.setAttribute('class', 'active')
+   const button = document.querySelector(`.button${page - 1}`).children[0]
+   button.setAttribute('class', 'active')
 }
-showPage(data, 1) 
+showPage(data, 1)
 
-
+//shows 9 people on the page and puts everything together
 function runPage(page) {
-         const firstIndex = parseInt(page) * 9 - 9;
-      const lastIndex = (parseInt(page) * 9) - 1;
-    const remove = document.querySelector('.active')
-    remove.setAttribute('class', 'undifined')
-    const button = document.querySelector(`.button${page - 1}`).children[0]
-     button.setAttribute('class', 'active')
-   	for(let i = 0; i < data.length; i++) {
-       LIfullStudentlist[i].style.display = 'none'
+   const firstIndex = parseInt(page) * 9 - 9;
+   const lastIndex = (parseInt(page) * 9) - 1;
+   const remove = document.querySelector('.active')
+   remove.setAttribute('class', 'undifined')
+   const button = document.querySelector(`.button${page - 1}`).children[0]
+   button.setAttribute('class', 'active')
+
+   //this is to set the display checks if its page 5 and subtract 3 from lastIndex to fix error 
+   if (button.tagName === 'BUTTON' && parseInt(button.textContent) === 5) {
+      for (let i = 0; i < data.length; i++) {
+         LIfullStudentlist[i].style.display = 'none';
       }
-     for (i = firstIndex; i <= lastIndex ; i++){
-     LIfullStudentlist[i].style.display = ''
-     }
+      for (i = firstIndex; i <= lastIndex - 3; i++) {
+         LIfullStudentlist[i].style.display = '';
+      }
+   } else {
+      for (let i = 0; i < data.length; i++) {
+         LIfullStudentlist[i].style.display = 'none';
+      }
+      for (i = firstIndex; i <= lastIndex; i++) {
+         LIfullStudentlist[i].style.display = '';
+      }
+   }
 
 }
 
-
+//adds button click event to run page function 
 ul.addEventListener('click', (e) => {
- const button = e.target;
-  if (button.tagName === 'BUTTON' && parseInt(button.textContent) <= 2000 ) {
+   const button = e.target;
+   if (button.tagName === 'BUTTON' && parseInt(button.textContent) <= 2000) {
       runPage(parseInt(button.textContent))
-         console.log(button.textContent);
+      console.log(button.textContent);
    } else {
       console.log('error')
    }
